c94088a7ca1fd4ef346528dfead4fa354d2eb594
AlaphaSimanga <95401433+AlaphaSimanga@users.noreply.github.com>
2022-05-15 17:31:00 +0200

    Add files via upload

---
+++ JS Arithmetic/js/Untitled-1.js
@@ -0,0 +1,49 @@
+// TITLE
+console.log(" ----- Area Of Shapes JS Arithmetic Challenge ----- \n\n")
+
+// Constant Variables:
+const pi = 3.1419;
+const half = 0.5;
+
+// Variables for measurements
+
+   /*
+   Delete these comments and
+   create your variables here..
+   */
+var rectangle, l=10, br=17;
+var circle,r=25;
+var triangle, b=20, h=9;
+
+// Rectangle area calculation
+   //replace with calculation code here..
+
+   rectangle =l*br;
+
+// Append your rectangle answer:
+console.log("The area of the given rectangle is : "+rectangle);
+
+
+console.log("\n");
+
+
+// circle area calculation
+   // replace with calculation code here..
+
+   circle=pi*r*r;
+
+// Append your circle answer:
+console.log("The area of the given circle is : "+circle);
+
+
+
+console.log("\n");
+
+
+// triangle area calculation
+   // replace with calculation code here..
+   
+   triangle=b*half*h;
+
+// Append your triangle answer:
+console.log("The area of the given triangle is : "+triangle);
